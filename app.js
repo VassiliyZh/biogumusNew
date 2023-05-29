07a4f7977cf431009de52133c49f361c5c29f1f9
VassiliyZh <119644334+VassiliyZh@users.noreply.github.com>
2023-05-29 17:17:44 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 function app() {
-    weight = parseInt(document.getElementById("weight").value);
-    height = parseInt(document.getElementById("height").value);
+    name1 = document.getElementById("name").value;
+    email = document.getElementById("email").value;
     
-    result = weight / (height*height/10000);
-   
-
-    switch (true) {
-        case result <= 16:
-            str = 'Выраженный дефицит массы тела';
-            break;
-        case result >16 && result <= 18.5:
-            str = 'Недостаточная масса тел';
-            break;
-        case result >18.5 && result <= 24.99:
-            str = 'Норма';
-            break;
-        case result >24.99:
-            str = 'Избыточная масса тела';
-            break;
-  
-        default:
-            str = 'Ошибка ввода';
-    }
-    document.getElementById("result").innerText = str;
+    document.getElementById("conclucion").innerText = "Мы рады видеть Вас на нашем сайте, " + name1;
+    document.getElementById("conclucion1").innerText = "Мы сохраним Ваш e-mail: " + email + " чтобы информировать Вас о самых последних новостях.";
 
 }
